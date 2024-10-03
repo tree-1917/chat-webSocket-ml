2839cf5248a37252d60ee9195b6fcab6a3a4796e
tree-1917 <gamalmo1917@gmail.com>
2024-10-03 18:17:02 +0300

    update main And add index.html

--- app/main.py
+++ app/main.py
@@ -1,51 +1,13 @@
 from fastapi import FastAPI, WebSocket, WebSocketDisconnect
 from fastapi.responses import HTMLResponse
 import json  # Import the json module
+from pathlib import Path
 
 app = FastAPI()
 
-html = """
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>Private Chat with WebSocket</title>
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    </head>
-    <body>
-        <div class="container mt-3">
-            <h1>Private Chat with WebSocket</h1>
-            <h2>Your ID: <span id="ws-id"></span></h2>
-            <form action="" onsubmit="sendMessage(event)">
-                <input type="text" class="form-control" id="targetId" placeholder="Enter target ID" autocomplete="off"/>
-                <input type="text" class="form-control mt-2" id="messageText" placeholder="Enter message" autocomplete="off"/>
-                <button class="btn btn-outline-primary mt-2">Send</button>
-            </form>
-            <ul id="messages" class="mt-5"></ul>
-        </div>
-        <script>
-            var client_id = Date.now();
-            document.querySelector("#ws-id").textContent = client_id;
-            var ws = new WebSocket(`ws://localhost:8000/ws/${client_id}`);
-            
-            ws.onmessage = function(event) {
-                var messages = document.getElementById('messages');
-                var message = document.createElement('li');
-                var content = document.createTextNode(event.data);
-                message.appendChild(content);
-                messages.appendChild(message);
-            };
-            
-            function sendMessage(event) {
-                var targetId = document.getElementById("targetId").value;
-                var input = document.getElementById("messageText").value;
-                var message = JSON.stringify({ "target_id": targetId, "message": input });
-                ws.send(message);
-                event.preventDefault();
-            }
-        </script>
-    </body>
-</html>
-"""
+# Load the HTML content from the index.html file
+html_file_path = Path(__file__).parent / "index.html"
+html_content = html_file_path.read_text()
 
 class ConnectionManager:
     def __init__(self):
@@ -67,7 +29,7 @@ manager = ConnectionManager()
 
 @app.get("/")
 async def get():
-    return HTMLResponse(html)
+    return HTMLResponse(content=html_content)
 
 @app.websocket("/ws/{client_id}")
 async def websocket_endpoint(websocket: WebSocket, client_id: int):
